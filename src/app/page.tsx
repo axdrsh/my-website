911bebd30aaa1bf460fce5594df1c02d3dcbc504
adarsh <163159242+axdrsh@users.noreply.github.com>
2024-11-17 22:23:16 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,28 +32,28 @@ export default function MinimalPersonalWebsite() {
         <ul className="list-disc pl-5">
           <li id="project">
             <a href="https://github.com/axdrsh/thoughtss" className="underline">
-              <samp>thoughts. - a place to let your thoughts out anonymously.</samp>
-            </a>
+              <samp>thoughts.</samp>
+            </a> <samp> - a place to let your thoughts out anonymously.</samp>
           </li>
           <li id="project">
             <a href="https://github.com/axdrsh/pulse" className="underline">
-              <samp>pulse - a chatbot that continously checks on you if you're studying. </samp>
-            </a>
+              <samp>pulse</samp>
+            </a> <samp> - a chatbot that continously checks on you if you're studying.</samp>
           </li>
           <li id="project">
             <a href="https://github.com/axdrsh/cusswatchbot" className="underline">
-              <samp>cusswatchbot - a discord bot that tracks the profanity usage of a user.</samp>
-            </a>
+              <samp>cusswatchbot</samp>
+            </a> <samp> - a discord bot that tracks the profanity usage of a user.</samp>
           </li>
           <li id="project">
             <a href="https://github.com/axdrsh/seekster" className="underline">
-              <samp>seekster - a google search clone</samp>
-            </a>
+              <samp>seekster</samp>
+            </a> <samp> - a google search clone.</samp>
           </li>
           <li id="project">
             <a href="https://github.com/axdrsh/solvetosurf" className="underline">
-              <samp>solvetosurf - a website blocker that requires you to solve math problems.</samp>
-            </a>
+              <samp>solvetosurf</samp>
+            </a> <samp> - a website blocker that requires you to solve math problems.</samp>
           </li>
         </ul>
         <br />
